b76b15eec82f7cf66567e160805e5df39811eafb
RobotGrrl <erin@robotgrrl.com>
2017-03-26 10:59:12 -0400

    Listening for key ins and send data

--- test-app/app2.js
+++ test-app/app2.js
@@ -1,17 +1,21 @@
 //var serialport = new SerialPort("/dev/cu.usbserial-AL00ESDG");
 //var serialport = new SerialPort("/dev/cu.usbserial-ADAOJONYd", {
 
+var sleep = require('sleep');
+var keypress = require('keypress');
 var SerialPort = require("serialport")
 
 var serialport = new SerialPort('/dev/cu.usbserial-ADAOJONYd', {
   parser: SerialPort.parsers.readline('\n')
 });
 
+keypress(process.stdin);
+
 serialport.on('open', function(){
   console.log('Serial Port Opened');
   
   serialport.on('data', function (data) {
-    console.log('Data: ' + data);
+    //console.log('Data: ' + data);
 
     if(data[0] === '<' && data[1] === '<') { // parse this message
       
@@ -42,6 +46,41 @@ serialport.on('open', function(){
     }
 
   });
+
+
+  process.stdin.on('keypress', function (ch, key) {
+    
+    if(key.name == 'a') {
+      for(var i=0; i<10; i++) {
+        console.log("sending A -------------------------"); 
+        serialport.write('A');
+        sleep.msleep(50);
+      }
+    } else if(key.name == 'b') {
+      for(var i=0; i<10; i++) {
+        console.log("sending B -------------------------"); 
+        serialport.write('B');
+        sleep.msleep(50);
+      }
+    } else if(key.name == 'c') {
+      for(var i=0; i<10; i++) {
+        console.log("sending C -------------------------"); 
+        serialport.write('C');
+        sleep.msleep(50);
+      }
+    } else if(key.name == 'd') {
+      for(var i=0; i<10; i++) {
+        console.log("sending D -------------------------"); 
+        serialport.write('D');
+        sleep.msleep(50);
+      }
+    } else if(key.name == 'e') {
+      console.log("sending E -------------------------");
+      serialport.write('E');
+    }
+
+  });
+
 
 });
 
